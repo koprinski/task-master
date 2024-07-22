21d98b029fe7001b66e2b9253b5ae3c7f592bae4
ilkdjan <ilkdjanismetov@gmail.com>
2024-07-22 14:31:17 +0000

    first steps

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -11,5 +11,17 @@ use App\Models\LongTermTask;
 
 class TaskController extends Controller
 {
-    //
+    public function habits(): \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory|\Illuminate\Foundation\Application
+    {
+        return view('tasks.habits');
+    }
+    public function dailyTasks(): \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory|\Illuminate\Foundation\Application
+    {
+        return view('tasks.dailyTasks');
+    }
+    public function longTermTasks(): \Illuminate\Contracts\View\View|\Illuminate\Contracts\View\Factory|\Illuminate\Foundation\Application
+    {
+        return view('tasks.longTermTasks');
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use App\Http\Controllers\TestController;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -13,9 +13,7 @@ Route::get('/', function () {
 Route::get('/bs', function () {
     return view('bs');
 });
-Route::get('/Habbits', function () {
-    return view('Habbits') ;
-}) -> name('Habbits');  ;
+Route::get('/habits', TaskController::class, 'habits')-> name('habits');  ;
 Route::get('/DailyTasks', function () {
     return view('Daily_tasks', ['pageName' => 'DailyTasks']);
 }) -> name('DailyTasks');
