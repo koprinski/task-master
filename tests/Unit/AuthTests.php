a4bf65c17377a520334d548e12ef35d4695ac68d
vagrant <vagrant@homestead>
2024-08-09 11:21:38 +0000

    some tests done

---
+++ tests/Unit/AuthTests.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use Tests\TestCase;
+
+
+class AuthTests extends TestCase
+{
+    /**
+     * A basic unit test example.
+     */
+    public function test_user_can_view_a_login_form()
+    {
+        $response = $this->get('/login');
+
+        $response->assertSuccessful();
+        $response->assertViewIs('auth.login');
+    }
+
+    public function test_user_can_view_a_register_form()
+    {
+        $response = $this->get('/register');
+
+        $response->assertSuccessful();
+        $response->assertViewIs('auth.register');
+    }
+
+    public function test_user_cannot_view_a_login_form_when_authenticated()
+    {
+        $user = User::factory()->make();
+
+        $response = $this->actingAs($user)->get('/login');
+
+        $response->assertRedirect('/habbit');
+    }
+
+    public function test_user_can_login_with_correct_credentials()
+    {
+        $user = User::factory()->create([
+            'password' => bcrypt($password = 'password'),
+        ]);
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => $password,
+        ]);
+
+        $response->assertRedirect('/habbit');
+        $this->assertAuthenticatedAs($user);
+    }
+    public function test_user_cannot_login_with_incorrect_password()
+    {
+        $user = User::factory()->create([
+            'password' => bcrypt('password'),
+        ]);
+
+        $response = $this->from('/login')->post('/login', [
+            'email' => $user->email,
+            'password' => 'invalid-password',
+        ]);
+
+        $response->assertRedirect('/login');
+        $response->assertSessionHasErrors('email');
+        $this->assertTrue(session()->hasOldInput('email'));
+        $this->assertFalse(session()->hasOldInput('password'));
+        $this->assertGuest();
+    }
+    public function test_remember_me_functionality()
+    {
+        $user = User::factory()->create([
+            'id' => random_int(1, 100),
+            'password' => bcrypt($password = 'i-love-laravel'),
+        ]);
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => $password,
+            'remember' => 'on',
+        ]);
+
+        $response->assertRedirect('/habbit');
+        $this->assertAuthenticatedAs($user);
+    }
+}
--- tests/Unit/CompletionTest.php
+++ tests/Unit/CompletionTest.php
@@ -4,14 +4,18 @@ namespace Tests\Unit;
 
 use App\Jobs\UpdateDailyTasks;
 use App\Models\DailyTask;
+use App\Models\User;
+use App\Services\UserService;
+use Illuminate\Support\Facades\Auth;
 use Tests\TestCase;
-
+use Illuminate\Foundation\Testing\RefreshDatabase;
 class CompletionTest extends TestCase
 {
+//    use RefreshDatabase;
     /**
      * A basic unit test example.
      */
-    public function test_checkDailyTaskCompletion(): void
+    public function test_checkDailyTaskCompletionCompleted(): void
     {
        $dailytasks = DailyTask::factory()->count(5)->completed()->create();
 
@@ -19,4 +23,39 @@ class CompletionTest extends TestCase
 
        $this->assertFalse($respone);
     }
+
+    public function test_checkDailyTaskCompletionNotCompleted(): void
+    {
+        $dailytasks = DailyTask::factory()->count(5)->create();
+
+        $respone = app(UpdateDailyTasks::class)->checkForUncompletedTasks($dailytasks);
+
+        $this->assertTrue($respone);
+    }
+
+    public function test_countUncompletedTasks(): void
+    {
+        $dailytasks = DailyTask::factory()->count(5)->create();
+
+        $response = app(UpdateDailyTasks::class)->countUncompletedTasks($dailytasks);
+
+        $this->assertEquals(5, $response);
+    }
+    public function test_countUncompletedTasksIfCompleted(): void
+    {
+        $dailytasks = DailyTask::factory()->count(5)->completed()->create();
+
+        $response = app(UpdateDailyTasks::class)->countUncompletedTasks($dailytasks);
+
+        $this->assertEquals(0, $response);
+    }
+
+//    public function test_changePointsH(): void
+//    {
+//        $user = User::factory()->make();
+//        $this->actingAs($user)
+//        $response = app(UserService::class)->changePointsH('+');
+//
+//        $this->assertEquals(600, $response);
+//    }
 }
