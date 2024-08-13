1a28842ab1e0c73159f685d834c495977dad6f4d
vagrant <vagrant@homestead>
2024-08-13 09:27:16 +0000

    changes to login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,6 +33,12 @@
         </div>
 
         <div class="flex items-center justify-end mt-4">
+
+
+                <a class="underline text-sm text-white hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 mr-3" href="{{ route('register') }}">
+                    Don't have an account?
+                </a>
+
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-white hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
